ad4409df2f37df140882458c55f133402b7a610a
Алекс Тырданов <alekstyrdanov@Alekss-Mac-mini.local>
2025-06-01 20:27:54 +0300

    #14 feat: add downloading pdf report

--- src/http/AnalyticApi.ts
+++ src/http/AnalyticApi.ts
@@ -5,4 +5,9 @@ export class AnalyticApi {
         const response = await analyticApi.get(`/analytic/category/${sender_id}`)
         return response.data
     }
+
+    static async generate_report(sender_id: number) {
+        const response = await analyticApi.get(`/report/${sender_id}`, { responseType: 'blob' })
+        return response.data
+    }
 }
